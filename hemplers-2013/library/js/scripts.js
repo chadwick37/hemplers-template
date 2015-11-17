b954c7f334ce61b532ba8c9abfb129d50de060d2
hnugroho <hnugroho@indo-soft.com>
2015-11-17 05:09:07 -0500

    adding script to add readmore

--- hemplers-2013/library/js/scripts.js
+++ hemplers-2013/library/js/scripts.js
@@ -415,8 +415,28 @@ TBD */
 TBD */
 
 
+// Adding Script readmore
+// Hide the extra content initially, using JS so that if JS is disabled, no problemo:
+$('.read-more-content').addClass('hide')
 
+// Set up a link to expand the hidden content:
+.before('<br /><a class="read-more-show" href="#">Read more...</a>')
+  
+// Set up a link to hide the expanded content.
+.append('<br /> <a class="read-more-hide" href="#">Show less</a>');
 
+// Set up the toggle effect:
+$('.read-more-show').on('click', function(e) {
+  $(this).next('.read-more-content').removeClass('hide');
+  $(this).addClass('hide');
+  e.preventDefault();
+});
+
+$('.read-more-hide').on('click', function(e) {
+  $(this).parent('.read-more-content').addClass('hide').parent().children('.read-more-show').removeClass('hide');
+  e.preventDefault();
+});
+// End Script readmore
 
 }); /* end of as page load scripts */
 
