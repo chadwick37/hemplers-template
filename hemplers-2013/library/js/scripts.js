a1d01b351b9bb3437af1750e593f4320a497d534
hnugroho <hnugroho@indo-soft.com>
2015-11-19 05:04:15 -0500

    update code

--- hemplers-2013/library/js/scripts.js
+++ hemplers-2013/library/js/scripts.js
@@ -423,7 +423,7 @@ $('.read-more-content').addClass('hide')
 .before('<br /><a class="read-more-show" href="#">Read more...</a>')
   
 // Set up a link to hide the expanded content.
-.append('<br /> <a class="read-more-hide" href="#">Show less</a>');
+.append('<br /><a class="read-more-hide" href="#">Show less</a>');
 
 // Set up the toggle effect:
 $('.read-more-show').on('click', function(e) {
