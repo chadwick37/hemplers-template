f0d9edfc7bddfedd80ae77b299941babb2452fa2
hnugroho <hnugroho@indo-soft.com>
2015-12-14 03:17:04 -0500

    update code to display video just on homepage

--- hemplers-2013/header.php
+++ hemplers-2013/header.php
@@ -80,6 +80,7 @@
 		<div id="container">
 			<header class="header" role="banner">
 
+			<?php if (is_front_page()) { ?>
 			<!-- Adding video on front page -->
 			<div id="vid-wrap">
                     	<canvas id="canvas-wrap"></canvas>
@@ -89,6 +90,7 @@
 						</video>
             </div>
 			<!-- end video on front page  -->
+			<?php }  ?>
 
 				<a href="http://forbaconssake.com" class="flag" id="baconlink"><img src="<?php echo $themepath; ?>/library/images/baconlink.png"></a>
 				<a href="/kestins-korner-2" class="flag" id="kestins-korner"><img src="<?php echo $themepath; ?>/library/images/kestins-korner.png"></a>
