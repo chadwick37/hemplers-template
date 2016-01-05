10df8e640120639902ee6bf1c0071d8fb3414879
hnugroho <hnugroho@indo-soft.com>
2016-01-04 22:51:59 -0500

    Kestins Korner: Header size Similar to footer – seems to be zoomed in. Kestins logo, photo, and font size for menu are too big.

--- hemplers-2013/category.php
+++ hemplers-2013/category.php
@@ -13,7 +13,7 @@
                 	while($contents->have_posts()):$contents->the_post();
 		                if ($post->ID == 511) {
 		                	echo '<div class="descriptions_blog" >';
-		                	echo "<img class='aligncenter' src='".get_stylesheet_directory_uri()."/library/images/kestins-korner-1.png' alt='kestins-korner-1' width='400' height='130' />";
+		                	echo "<img class='aligncenter' src='".get_stylesheet_directory_uri()."/library/images/kestins-korner-12.png' alt='kestins-korner-1' />";
 		                	the_content(); 
 		                	echo '</div>';
 		                }
